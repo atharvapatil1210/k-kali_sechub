73b222ebcec28d9fa79dec5176ff54e064e7b882
UnsettledAverage73 <145995913+atharvapatil1210@users.noreply.github.com>
2025-01-28 15:13:49 +0530

    Create wc.sh

---
+++ wc.sh
@@ -0,0 +1,56 @@
+cat > ssgmce.txt #add
+
+# Add text
+# ctrl + z
+
+cat >> ssgmce.txt #append
+
+# add text
+#ctrl +z 
+
+wc ssgmce.txt
+
+#line words charcter 
+
+sort ssgmce.txt
+
+#send o/p to another file
+sort ssgmce.txt > pqr.txt
+
+#display content of file
+cat abc.txt xyz.txt 
+
+#trasfter o/p to another file
+cat abc.txt xyz.txt > lmn.txt
+
+
+#append o/p to another file
+cat abc.txt xyz.txt >> lmn.txt
+
+#head cmd 
+head -2 ssgmce.txt # shown first line
+
+tail -2 ssgmce.txt # shown last line
+
+ls -l 
+#d for directory | l for link | rwx for read write and execute
+
+# owner and user and no. of character and date and filename
+
+ls -a 
+#hidden file 
+#rename with .ssgmce.txt
+
+ls -al 
+#long listing of files
+
+ls -al | head -3
+# display only 3 line
+
+ls -al | tail -5 #display last 5 line
+
+#shown 4 to 6 file display
+ls -l | head 
+
+
+
